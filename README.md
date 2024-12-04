d8469ae5e18a66d863807464e58f0026da5dce4a
peterjwms <peterjwms@gmail.com>
2024-12-04 12:55:05 -0500

    finish collecting datasets

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ COSI 135b Final Project exploring contextual embeddings with semantic and syntac
  - Pipeline for tagging datasets with syntactic information
  - Pipeline for tagging datasets with semantic information
  - Finetuning architecture/pipeline
+
+
+## Dataset notes/descriptions
+### [MNLI GLUE](https://huggingface.co/datasets/nyu-mll/glue)
+ - matched is on in-domain data, mismatched is cross-domain
+ - labels are probably:
+   - -1 = contradiction
+   - 0 = neutral
+   - 1 = entailment
+
+
+### [TruthfulQA](https://huggingface.co/datasets/truthfulqa/truthful_qa)
+ - mc1 is where only one answer to select from is correct
+ - mc2 is (probably at least) one or more correct answers to select from
--- collect_datasets.py
+++ collect_datasets.py
@@ -1,16 +1,21 @@
-from huggingface_hub import hf_hub_download
-from datasets import load_dataset
+# from huggingface_hub import hf_hub_download
+# from datasets import load_dataset
 from pathlib import Path
 
-ds = load_dataset("truthfulqa/truthful_qa", "generation")
+# ds = load_dataset("truthfulqa/truthful_qa", "generation")
 
-print(ds)
+# print(ds)
 
 import pandas as pd
 
 df = pd.read_parquet("hf://datasets/truthfulqa/truthful_qa/generation/validation-00000-of-00001.parquet")
-
-print(df.head())
-print(df)
-
 df.to_csv(Path("datasets/truthfulqa_generation.csv"), index=False)
+
+df = pd.read_parquet("hf://datasets/truthfulqa/truthful_qa/multiple_choice/validation-00000-of-00001.parquet")
+df.to_csv(Path("datasets/truthfulqa_multiple_choice.csv"), index=False)
+
+
+splits = {'train': 'mnli/train-00000-of-00001.parquet', 'validation_matched': 'mnli/validation_matched-00000-of-00001.parquet', 'validation_mismatched': 'mnli/validation_mismatched-00000-of-00001.parquet', 'test_matched': 'mnli/test_matched-00000-of-00001.parquet', 'test_mismatched': 'mnli/test_mismatched-00000-of-00001.parquet'}
+for split in splits.keys():
+    df = pd.read_parquet("hf://datasets/nyu-mll/glue/" + splits[split]) 
+    df.to_csv(Path(f"datasets/mnli_{split}.csv"), index=False)
