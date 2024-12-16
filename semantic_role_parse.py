85e87fd0674867405cb1d7300cae306eb49d8774
jaremsaunders <jarem.saunders@gmail.com>
2024-12-15 23:23:55 -0500

    updated semantic parser

--- semantic_role_parse.py
+++ semantic_role_parse.py
@@ -4,47 +4,56 @@ import ast
 import csv
 import json
 import pandas as pd
+import sys
+
+orig_stdout = sys.stdout
 
 
-def parse_sentence(sentence, parser,semantic=True,syntax=True):
+
+def parse_sentence(sentence, nlp,semantic=True,syntax=True,lemmatize=False):
+    #print('making nlp')
+    #print(sentence)
     try:
-        doc = parser(sentence)
+        doc = nlp(sentence)
 
     except:
         print("Failed to process ",sentence)
-        return
-
-    
-    processed_tokens = [token.lemma_ for token in doc]
-    tokens_len = len(processed_tokens)
+    #print('made nlp')
+    if lemmatize:
+        processed_tokens = [token.lemma_ for token in doc if token.pos_ != 'PUNCT']
+    else:
+        processed_tokens = [token.text for token in doc if token.pos_ != 'PUNCT']
     if syntax:
         syntax_tags = [token.pos_ for token in doc]
         processed_tokens = [lemma + '_' + pos for lemma, pos in zip(processed_tokens,syntax_tags)]
     if semantic:
-        semantic_roles = getSemTags(sentence,tokens_len)
-        processed_tokens = [lemma + '_' + role for lemma, role in zip(processed_tokens,semantic_roles)]
+        semantic_roles = getSemTags(sentence)
+        processed_tokens = [lemma + role for lemma, role in zip(processed_tokens,semantic_roles)]
     return processed_tokens
 
-def getSemTags(sentence,tokens_len):
+def getSemTags(sentence):
+    num_tokens = len(sentence.split(" "))
     sentence = sentence.replace(' ','%20')
+    #print('requests req')
     try:
         res = requests.get('http://localhost:8080/predict/semantics?utterance='+ sentence)
     except:
         print("failed to GET ", sentence)
+    #print('got resp')
     json = res.json()
-    props = json['props']
 
-    tags = ['unk'] * tokens_len
-    for prop in res.json()['props']:
-        for span in prop['spans']:
-            for i in range(span['start'],min(span['end']+1,tokens_len)):
-                tags[i] = span['vn']
+    tags = [''] * num_tokens
+    for prop in json['props']:
+        for span in prop['spans']: #still need to identify if isPredicate
+            for i in range(span['start'],min(span['end']+1,num_tokens)):
+                tags[i] = tags[i] + "_" + span['label'].replace(" ","").replace("<->","_")
     return tags
 
-processed_data = []
+
 print("start")
 
-parser = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_sm")
+nlp.tokenizer.rules = {key: value for key, value in nlp.tokenizer.rules.items() if "'" not in key and "’" not in key and "‘" not in key} #modifies pipeline so it doesn't split on apostrophes
 
 # df = pd.read_csv('datasets/truthfulqa_generation.csv')
 
@@ -65,23 +74,27 @@ parser = spacy.load("en_core_web_lg")
 
 
 
-
-
+processed_data = []
 with open('datasets/truthfulqa_generation.csv', 'r') as file:
+    print('opened file')
     csv_reader = csv.reader(file)
     next(csv_reader, None) 
-    for row in csv_reader:
+    file_data = [row for row in csv_reader]
+
+
+for row in file_data:
         output = {}
         output["adversarial"] = row[0]
         output["category"] = row[1]
-        output["question"] = parse_sentence(row[2],parser)
-        output["answer"] = parse_sentence(row[3],parser)
-        output["alt_answers"] =  [parse_sentence(alt_answer,parser) for alt_answer in ast.literal_eval(row[4].replace("\n",","))]
-        output["wrong_answers"] =  [parse_sentence(wrong_answer,parser) for wrong_answer in ast.literal_eval(row[5].replace("\n",","))]
+        output["question"] = parse_sentence(row[2],nlp)
+        output["answer"] = parse_sentence(row[3],nlp)
+        output["alt_answers"] =  [parse_sentence(alt_answer,nlp) for alt_answer in ast.literal_eval(row[4].replace("\n",","))]
+        output["wrong_answers"] =  [parse_sentence(wrong_answer,nlp) for wrong_answer in ast.literal_eval(row[5].replace("\n",","))]
         processed_data.append(json.dumps(output))
+        # print(json.dumps(output))
 
 
-with open('out.json', 'w') as file:
+with open('qa_gen_processed.json', 'w') as file:
     json.dump(processed_data, file, indent=4)
 
 print("made it to the end")
